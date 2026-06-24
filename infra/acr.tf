152a5d998bd74c4411bc08e9d497fb70e5ad77c6
cmomodo <ceesay.ml@outlook.com>
2026-06-24 15:30:15 +0100

    This is my first comment.The system design is done.We will be working on the Docker file very soon.Containerize first, then deploy into the cloud next.

--- infra/acr.tf
+++ infra/acr.tf
@@ -1,10 +1,7 @@
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "example" {
-  name     = "example-rg"
-  location = "West Europe"
-}
+
 resource "azurerm_container_registry" "example" {
   name                  = "exampleacr"
   resource_group_name   = azurerm_resource_group.example.name
